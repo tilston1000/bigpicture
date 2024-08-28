c6bbd19b36b11cdb5c8b4daba91ca8a6c2848276
tilston1000 <30899582+tilston1000@users.noreply.github.com>
2024-08-28 11:32:09 +0100

    Update Startup.cs

--- TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
+++ TailwindTraders.Website/Source/Tailwind.Traders.Web/Startup.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Tailwind.Traders.Web.Standalone;
-
+// tirgger a PR verification build
 namespace Tailwind.Traders.Web
 {
     public class Startup
